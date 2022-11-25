d16f590d717901536ef6f291c1a9476db3785c2b
Scorpion564 <ds1008076@gmail.com>
2022-11-25 21:56:58 +0530

    updated

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("open-popup").addEventListener("click", function name(params) {
+    document.getElementsByClassName("popup") [0].classList.add("active");
+});
+
+document.getElementById("dismiss-btn").addEventListener("click", function name(params) {
+    document.getElementsByClassName("popup")
+    [0],this.classList.remove("active");
+});
